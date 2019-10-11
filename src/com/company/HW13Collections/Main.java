b9f7e2ab2fda8a7c20c55c14555368e986b7aeea
ShBr2k01 <ShBr2k@gmail.com>
2019-10-11 11:47:02 +0300

    001

---
+++ src/com/company/HW13Collections/Main.java
@@ -0,0 +1,59 @@
+package com.company.HW13Collections;
+
+
+import com.sun.xml.internal.bind.v2.runtime.AssociationMap;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Now allow null keys
+        Map personMap = new TreeMap();
+        personMap.put("LN01", "FN00");
+        personMap.put("LN02", "FN02");
+        personMap.put("LN03", "FN03");
+        personMap.put("LN04", "FN04");
+        personMap.put("LN05", "FN05");
+        personMap.put("LN06", "FN06");
+        personMap.put("LN07", "FN07");
+        personMap.put("LN08", "FN08");
+        personMap.put("LN09", "FN09");
+        personMap.put("LN10", "FN00");
+        //System.out.println(personMap.keySet());
+        //System.out.println(personMap.values());
+        //for (Object entry: personMap.entrySet()) {
+
+        //    System.out.println(entry);
+
+        //}
+
+        for (Object entry: personMap.entrySet()) {
+
+            System.out.println(entry);
+
+            int count = Collections.frequency(new ArrayList<String>(personMap.values()), personMap.values());
+            System.out.println(count);
+
+        }
+
+        /*
+        Map<String, String> result = new TreeMap<String, String>();
+        for (Map.Entry<String, String> entry:personMap.entrySet()) {
+            String value = entry.getValue();
+            Integer count = result.get(value);
+            if (count == null)
+                result.put(value, new Integer(1));
+            else
+                result.put(value, new Integer(count+1));
+        }
+        */
+
+
+
+    }
+}
