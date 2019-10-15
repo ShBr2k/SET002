b82d49f717579ee9c62a5d61b68a69e38573ea00
ShBr2k01 <ShBr2k@gmail.com>
2019-10-15 11:26:56 +0300

    001

--- src/com/company/HW14Collection/Main.java
+++ src/com/company/HW14Collection/Main.java
@@ -7,16 +7,15 @@ import java.util.Iterator;
 
 public class Main {
 
-
     public static void main(String[] args) {
 
         ArrayList<Student> arrayListStudent = new ArrayList<Student>();
 
-        Student student01 = new Student("Name_0C", 2);
-        Student student02 = new Student("Name_0E", 1);
-        Student student03 = new Student("Name_0A", 5);
-        Student student04 = new Student("Name_0D", 6);
-        Student student05 = new Student("Name_0B", 2);
+        Student student01 = new Student("Name_01", 2);
+        Student student02 = new Student("Name_02", 1);
+        Student student03 = new Student("Name_03", 5);
+        Student student04 = new Student("Name_02", 6);
+        Student student05 = new Student("Name_05", 2);
 
         arrayListStudent.add(student01);
         arrayListStudent.add(student02);
@@ -24,23 +23,32 @@ public class Main {
         arrayListStudent.add(student04);
         arrayListStudent.add(student05);
 
-        // Sorting by Name
-        System.out.println("Sort by Name:");
-        Collections.sort(arrayListStudent, new NameComparator());
+        // Print Students by default
+        System.out.println(System.lineSeparator() + "Print Students by default:");
         Student.printStudents(arrayListStudent, 0);
 
+        // Compare by Name;
+        System.out.println(System.lineSeparator() + "Compare by Name: ");
+        Student.CompareName(arrayListStudent);
+
+        // Compare by Course;
+        System.out.println(System.lineSeparator() + "Compare by Course: ");
+        Student.CompareCourse(arrayListStudent);
+
+        // Sorting by Name
+        System.out.println(System.lineSeparator() + "Sort by Name:");
+        Collections.sort(arrayListStudent, new NameComparator());
+        Student.printStudents(arrayListStudent, 0);
 
         // Sorting by Course
         System.out.println(System.lineSeparator() + "Sort by Course:");
         Collections.sort(arrayListStudent, new CourseComparator());
         Student.printStudents(arrayListStudent, 0);
 
-
         // Using a iterator
         int course = 2;
         System.out.println(System.lineSeparator() + "Select Course: " + course);
         Student.printStudents(arrayListStudent, course);
-
     }
 
 }
--- src/com/company/HW14Collection/Student.java
+++ src/com/company/HW14Collection/Student.java
@@ -15,7 +15,7 @@ public class Student {
     }
 
 
-    //Print
+    //Print Students
     public static void printStudents(ArrayList<Student> arrayListStudent, int course) {
 
         Iterator<Student> iteratorStudent = arrayListStudent.iterator();
@@ -33,21 +33,56 @@ public class Student {
     }
 
 
+    //Compare by Name
+    public static void CompareName(ArrayList<Student> arrayListStudent) {
+        for (int i = 0; i < arrayListStudent.size(); i++) {
+            String firstName = arrayListStudent.get(i).Name;
+            int firstCourse = arrayListStudent.get(i).Course;
+            for (int j = i + 1; j < arrayListStudent.size(); j++) {
+                String secondName = arrayListStudent.get(j).Name;
+                int secondCourse = arrayListStudent.get(j).Course;
+                if (secondName.equals(firstName)) {
+                    System.out.println("** Matched: " + firstName + " - " + firstCourse + " : " + secondName + " - " + secondCourse);
+                } else {
+                    System.out.println("Mismatched: " + firstName + " - " + firstCourse + " : " + secondName + " - " + secondCourse);
+                }
+            }
+        }
+    }
+
+
+    //Compare by Course
+    public static void CompareCourse(ArrayList<Student> arrayListStudent) {
+        for (int i = 0; i < arrayListStudent.size(); i++) {
+            String firstName = arrayListStudent.get(i).Name;
+            int firstCourse = arrayListStudent.get(i).Course;
+            for (int j = i + 1; j < arrayListStudent.size(); j++) {
+                String secondName = arrayListStudent.get(j).Name;
+                int secondCourse = arrayListStudent.get(j).Course;
+                if (secondCourse == firstCourse) {
+                    System.out.println("** Matched: " + firstCourse + " - " + firstName + " : " + secondCourse + " - " + secondName);
+                } else {
+                    System.out.println("Mismatched: " + firstCourse + " - " + firstName + " : " + secondCourse + " - " + secondName);
+                }
+            }
+        }
+    }
+
+
     @Override
     public String toString() {
         return this.Name + " - " + this.Course;
     }
 
 
-
     public String getName() {
         return this.Name;
     }
 
+
     public int getCourse() {
         return this.Course;
     }
-
 
 
 }
