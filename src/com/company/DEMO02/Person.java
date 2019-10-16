f2174aacaa8d34a958068842daadbe82d206041d
ShBr2k01 <ShBr2k@gmail.com>
2019-10-16 20:04:40 +0300

    001

--- src/com/company/DEMO02/Person.java
+++ src/com/company/DEMO02/Person.java
@@ -4,23 +4,50 @@ import java.util.Date;
 
 public class Person {
     private int TaxNumber;
-    private String firstname;
-    private String lastname;
-    private Date birthdate;
+    private String Firstname;
+    private String Lastname;
+    private Date Birthdate;
 
 
-    public Person(int taxNumber, String firstname, String lastname, Date birthdate){
+    public Person(int taxNumber, String firstName, String lastName, Date birthDate) {
+        this.TaxNumber = taxNumber;
+        this.Firstname = firstName;
+        this.Lastname = lastName;
+        this.Birthdate = birthDate;
+    }
 
+    public void setTaxNumber(int taxNumber) {
+        this.TaxNumber = taxNumber;
+    }
+
+    public void setFirstname(String firstname) {
+        this.Firstname = firstname;
+    }
+
+    public void setLastname(String lastname) {
+        this.Lastname = lastname;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.Birthdate = birthdate;
+    }
+
+    public int getAge() {
+        return 0; //return age
     }
 
     public void Input() { //not void?
-
+        //add reader method from zTools
     }
 
     public void Output() { //not void?
 
     }
 
+    @Override
+    public String toString() {
+        return "TaxNumber: " + this.TaxNumber + ". First Name: " + this.Firstname + ". Last Name: " + this.Lastname + ". Last Name: " + this.Birthdate;
+    }
 
 
 }
--- src/com/company/DEMO02/Student.java
+++ src/com/company/DEMO02/Student.java
@@ -1,4 +1,25 @@
 package com.company.DEMO02;
 
-public class Student extends Person {
+
+import java.util.Date;
+
+public class Student extends Person{
+
+    public Student (int taxNumber, String firstName, String lastName, Date birthDate)
+    {
+        super(taxNumber, firstName, lastName, birthDate);
+    }
+
+
+
 }
+
+
+/*
+public  Student extends Person {
+
+    public boo
+
+}
+
+*/
