98a607fb1689049b3a42dd3b2567a7e8043a51c5
ShBr2k01 <ShBr2k@gmail.com>
2019-10-16 16:30:19 +0300

    001

---
+++ src/com/company/DEMO02/Person.java
@@ -0,0 +1,26 @@
+package com.company.DEMO02;
+
+import java.util.Date;
+
+public class Person {
+    private int TaxNumber;
+    private String firstname;
+    private String lastname;
+    private Date birthdate;
+
+
+    public Person(int taxNumber, String firstname, String lastname, Date birthdate){
+
+    }
+
+    public void Input() {
+
+    }
+
+    public void Output() {
+
+    }
+
+
+
+}
---
+++ src/com/company/DEMO02/Student.java
@@ -0,0 +1,4 @@
+package com.company.DEMO02;
+
+public class Student extends Person {
+}
