c918db6b5e7c12813c79f253400ea81af4017e46
ShBr2k <shbr2k@gmail.com>
2019-10-20 10:25:28 +0300

    Merge branch 'master' of https://github.com/ShBr2k/SET002 into Demo02
    # Conflicts:
    #	src/com/company/DEMO02/FirstNameComparator.java
    #	src/com/company/DEMO02/LastNameComparator.java
    #	src/com/company/DEMO02/Main.java

--- src/com/company/DEMO02/PersonAgeTest.java
+++ src/com/company/DEMO02/PersonAgeTest.java
@@ -1,5 +1,6 @@
 package com.company.DEMO02;
 
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -10,13 +11,13 @@ import java.text.ParseException;
 import static com.company.DEMO02.Settings.dateFormat;
 
 
-public class PersonTest {
+public class PersonAgeTest {
 
     Person person1 = null;
     Person person2 = null;
 
     @BeforeTest
-    public void initStringsAndObjects() {
+    public void initObjects() {
         try {
             person1 = new Person("TN001", "FN0D", "LN0A", dateFormat.parse("20.07.1979"));
         } catch (ParseException e) {
@@ -40,12 +41,22 @@ public class PersonTest {
 
     }
 
+
     @Test(priority = 1)
     public void testGetAge2() {
 
         int expected = 39;
         int actual = person2.getAge();
         assertEquals(actual, expected);
+
+    }
+
+
+    @AfterTest
+    public void cleanup() {
+
+        person1 = null;
+        person2 = null;
 
     }
 }
---
+++ src/com/company/DEMO02/SerializationTest.java
@@ -0,0 +1,76 @@
+package com.company.DEMO02;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.ArrayList;
+
+import static com.company.DEMO02.Deserialization.DeserializeArrayListPersonFromXML;
+import static com.company.DEMO02.Serialization.SerializeArrayListPersonToXML;
+import static com.company.DEMO02.Settings.dateFormat;
+import static org.testng.Assert.*;
+
+public class SerializationTest {
+
+    ArrayList<Person> serializationArrayListPerson = new ArrayList<>();
+    ArrayList<Person> deserializeArrayListPerson = new ArrayList<>();
+
+    @BeforeTest
+    public void initObjects() {
+
+        // Add object in ArrayListPerson
+        try {
+            serializationArrayListPerson.add(new Person("TN001", "FN0D", "LN0A", dateFormat.parse("20.07.1979")));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        // Serialization
+        try {
+            SerializeArrayListPersonToXML(serializationArrayListPerson);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+
+        // Deserialization
+        try {
+            deserializeArrayListPerson = DeserializeArrayListPersonFromXML();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+    }
+
+
+    @Test(priority = 1)
+    public void testSizeSerializeArrayListPerson() {
+
+        int expected = serializationArrayListPerson.size();
+        int actual = deserializeArrayListPerson.size();
+        assertEquals(actual, expected);
+
+    }
+
+
+    @Test(priority = 2)
+    public void testValueAgeSerializeArrayListPerson() {
+
+        int expected = serializationArrayListPerson.get(0).getAge();
+        int actual = deserializeArrayListPerson.get(0).getAge();
+        assertEquals(actual, expected);
+
+    }
+
+
+    @AfterTest
+    public void cleanup() {
+
+        serializationArrayListPerson = null;
+        deserializeArrayListPerson = null;
+
+    }
+}
